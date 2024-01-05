4a1f8f41ababfe91aafbadd25f714f0ac67dde8f
Thanh_1203 <thangcm@edtexco.com>
2024-01-05 23:36:08 +0700

    base

--- Fe-website/src/main.ts
+++ Fe-website/src/main.ts
@@ -1,14 +1,22 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 
-const app = createApp(App)
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 
-app.use(router);
+import "./assets/styles/tailwind.css"
+import "./style.scss"
+
+library.add(faUserSecret);
+const app = createApp(App);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
+// app.use(router);
 app.use(Antd);
 app.mount('#app');
