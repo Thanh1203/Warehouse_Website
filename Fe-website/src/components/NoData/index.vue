532312d860f58fa1950157208e9961be8d5188ac
Thanh_1203 <thangcm@edtexco.com>
2024-01-13 15:43:41 +0700

    base

---
+++ Fe-website/src/components/NoData/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="tw-py-4 tw-font-bold tw-overflow-hidden">
+    <!-- <Empty class="shake" :description="emptyText || translate('noData')" /> -->
+    <Empty class="shake" :description="emptyText"/>
+  </div>
+</template>
+<script setup lang="ts">
+import { Empty } from 'ant-design-vue'
+// import { translate } from '@/languages/i18n'
+const props = defineProps({
+  emptyText: {
+    type: String
+  }
+})
+</script>
+<style lang="scss" scoped>
+.shake {
+  animation: shake 0.2s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+  animation-iteration-count: 2;
+}
+
+@keyframes shake {
+  0% {
+    transform: rotate(0deg);
+  }
+  25% {
+    transform: rotate(5deg);
+  }
+  50% {
+    transform: rotate(0eg);
+  }
+  75% {
+    transform: rotate(-5deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+}
+</style>
