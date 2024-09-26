cfa5349104706c94f4358971123aa7d91e22f73d
Thanh1203 <ducthanh1122003@gmail.com>
2024-09-26 15:55:17 +0700

    update 26-09-24

--- BackendWebApi/BackendWebApi/DTOS/DTOWarehouse_Import.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BackendWebApi.DTOS
-{
-    public class DTOWarehouse_Import
-    {
-        public string? Code { get; set; }
-        public int TotalProduct { get; set; }
-        public string? TimeCreate { get; set; }
-    }
-}
--- BackendWebApi/BackendWebApi/Interfaces/IWarehouse_Export.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BackendWebApi.Interfaces
-{
-    public interface IWarehouse_Export
-    {
-        Task<object> GetWeareHouseExport(int warehouseId, int companyId);
-        Task<object> SearchWarehouseExport(string day, string month, string year, int warehouseId, int companyId);
-    }
-}
